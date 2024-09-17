f9c91bcc52999f5ae9499e5dd8001efb5039b2f0
Ignacio Bailo <bailoignacio29@gmail.com>
2024-09-17 11:55:16 -0300

    Logins y Logouts terminados

--- proyecto/core/urls.py
+++ proyecto/core/urls.py
@@ -1,9 +1,12 @@
-
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = "core"
 
 urlpatterns = [
-   path("", views.index, name="index")
+   path("", views.index, name="index"),
+   path("login", LoginView.as_view(template_name="core/login.html"), name="login"),
+   path('logout', LogoutView.as_view(template_name="core/logout.html"), name='logout'),
+
 ]
